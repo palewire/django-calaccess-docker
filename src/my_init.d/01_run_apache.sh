8666f74aa83dc0d9072a8bb3525a3038fbf78fa6
palewire <ben.welsh@gmail.com>
2015-01-25 17:51:32 -0800

    Version 0.4 with ulimit flail

--- src/my_init.d/01_run_apache.sh
+++ src/my_init.d/01_run_apache.sh
@@ -5,4 +5,5 @@ echo "export MYSQL_DATABASE=$MYSQL_DATABASE" >> /etc/apache2/envvars
 echo "export MYSQL_USER=$MYSQL_USER" >> /etc/apache2/envvars
 echo "export MYSQL_PASSWORD=$MYSQL_PASSWORD" >> /etc/apache2/envvars
 source /etc/apache2/envvars
+ulimit -n 10000
 sudo service apache2 restart
