c9faf98aee67d0bf1a67eaf992d7c09a41112147
palewire <ben.welsh@gmail.com>
2015-01-25 22:10:40 -0800

    MySQL config changes for 0.6

--- src/my_init.d/02_run_mysql.sh
+++ src/my_init.d/02_run_mysql.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
+
+# Listen on all interfaces
+sed -i "s#^bind-address\s*=\s*.*#bind-address = 0.0.0.0#" /etc/mysql/my.cnf
+
+# Log to stdout (remove logfile entry)
+sed -i 's#^log_error = /var/log/mysql/error.log#console#' /etc/mysql/my.cnf
+
+# Key_buffer_size is deprecated
+sed -i 's#key_buffer_size =#key_buffer =#g' /etc/mysql/my.cnf
+
+# Start it up
 sudo service mysql restart;
